936c504bc04bc7ffdac95f91455f3b0041eeedc2
Rob Williams <rob@ontometrics.com>
2017-05-23 23:30:23 -0700

    Added ability to parse YAML.

---
+++ src/test/kotlin/com/ontometrics/kstaticgen/YAMLParserSpec.kt
@@ -0,0 +1,57 @@
+package com.ontometrics.kstaticgen
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import mu.KotlinLogging
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.test.assertNotNull
+
+/**
+ *
+ *
+ * User: Rob
+ * Date: 5/23/17
+ * Time: 11:22 PM
+ *
+ * (c) ontometrics 2017, All Rights Reserved
+ */
+object YAMLParserSpec : Spek({
+
+    val log = KotlinLogging.logger {}
+
+    describe("basic parsing of existing content"){
+        on("some simple front matter YAML"){
+            val universeConfigYamlFilePath = Paths.get("src/test/resources/YAML/universe-config.yaml")
+            val loaded = loadFromFile(universeConfigYamlFilePath)
+            it("should be possible to parse"){
+                log.debug { "YAML: $loaded" }
+                assertNotNull(loaded)
+            }
+        }
+
+    }
+})
+
+data class UniverseSizeDto(val maxGalaxies: Int, val maxSystems: Int, val maxPlanets: Int)
+
+data class ResourcesDto(val crystal: Int, val gas: Int, val energy: Int)
+
+data class StarterPlanetDto(val resources: ResourcesDto)
+
+data class ConfigDto(val universeSize: UniverseSizeDto, val starterPlanet: StarterPlanetDto, val roundTime: Int)
+
+fun loadFromFile(path: Path): ConfigDto {
+    val mapper = ObjectMapper(YAMLFactory()) // Enable YAML parsing
+    mapper.registerModule(KotlinModule()) // Enable Kotlin support
+
+    return Files.newBufferedReader(path).use {
+        mapper.readValue(it, ConfigDto::class.java)
+    }
+}
